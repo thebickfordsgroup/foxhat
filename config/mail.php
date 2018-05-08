4527da5ef401c2af5e356a07f6a04a6d5dc64b99
Craig Davies <cdavies@internal.bickfords.com.au>
2018-05-08 17:09:52 +0930

    Updates to css

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 2525),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('f7ea208f168fa4'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('2a92c931af5667'),
 
     /*
     |--------------------------------------------------------------------------
