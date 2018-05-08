58e2805dbe5baef2e104892d221a703a0bd34667
Craig Davies <cdavies@internal.bickfords.com.au>
2018-05-08 14:46:45 +0930

    added alliases

--- config/app.php
+++ config/app.php
@@ -191,6 +191,8 @@ return [
         'Event' => Illuminate\Support\Facades\Event::class,
         'File' => Illuminate\Support\Facades\File::class,
         'Form' => 'Collective\Html\FormFacade',
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
