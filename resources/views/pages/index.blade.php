4fcc5b5e91f0cdbdca136f8fa9c5fef0623e1705
Craig Davies <cdavies@internal.bickfords.com.au>
2018-05-08 10:49:36 +0930

    Update git ignore

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -87,14 +87,7 @@
       
       function initAutocomplete() {
 
-      var locations = 
-      [
-        ['Bondi Beach', -33.890542, 151.274856, 4],
-        ['Coogee Beach', -33.923036, 151.259052, 5],
-        ['Cronulla Beach', -34.028249, 151.157507, 3],
-        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-        ['Maroubra Beach', -33.950198, 151.259302, 1]
-      ];
+      @include('partials.stockists')
 
         var map = new google.maps.Map(document.getElementById('map'), 
         {
@@ -180,7 +173,7 @@
         }
       })(marker, i));
     }
-        
+
       }
 
 
