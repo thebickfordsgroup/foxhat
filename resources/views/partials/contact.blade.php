5af1b54f0f305ccd4e6f6731c6ded92fb41dfb5f
Craig Davies <cdavies@internal.bickfords.com.au>
2019-03-08 14:02:33 +1030

    new stockists

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -1,8 +1,8 @@
 			<form action="{{  url('contact') }}" method="POST">	
 				{{ csrf_field() }}	
 				<div class="container">	
-					<div class="col-md-3"></div>
-					<div class="col-md-3">
+					<div class="col-md-2"></div>
+					<div class="col-md-4">
 			  			<div class="form-group">
 					    	<input type="text" class="contact" name="name" id="name" placeholder="NAME" required>
 				  		</div>
@@ -10,7 +10,7 @@
 					    	<input type="text" class="contact" name="postcode" id="postcode" placeholder="POSTCODE" required>
 					  	</div>	
 			  		</div>		
-					<div class="col-md-3">
+					<div class="col-md-4">
 			  			<div class="form-group">
 					    	<input type="email" class="contact" name="email" id="email" placeholder="EMAIL" required>
 				  		</div>
@@ -18,24 +18,24 @@
 					    	<input type="text" class="contact" name="phone" id="phone" placeholder="PHONE" required>
 					  	</div>	
 			  		</div>	
-					<div class="col-md-3"></div>
+					<div class="col-md-2"></div>
 			  	</div>
 			  	<div class="container">				  			
-					<div class="col-md-3"></div>
-			  		<div class="col-md-6">
+					<div class="col-md-2"></div>
+			  		<div class="col-md-8">
 			  			<div class="form-group">
 			  			 	<textarea class="contact" name="message" placeholder="YOUR MESSAGE" required></textarea>
 			  			</div>
 			  		</div>	
-					<div class="col-md-3"></div>
+					<div class="col-md-2"></div>
 				</div>
 				<div class="container">				  			
-					<div class="col-md-5"></div>
-			  		<div class="col-md-2">
+					<div class="col-md-4"></div>
+			  		<div class="col-md-4">
 			  			<div style="text-align: center;">
 			  				<button type="submit" class="contact">SUBMIT</button>
 			  			</div>
 					</div>	
-					<div class="col-md-5"></div>
+					<div class="col-md-4"></div>
 				</div>
 			</form>
